67562a0ebe3c490e12fb9f46c62f4bc2db500be8
Denis <carriere.denis@gmail.com>
2017-06-04 15:23:44 -0400

    Update prefix

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,2 +1,2 @@
-wget http://download.geofabrik.de/north-america/us/florida-latest.osm.pbf -p data/
+wget http://download.geofabrik.de/north-america/us/florida-latest.osm.pbf -P data/
 osmupdate "data/florida-latest.osm.pbf" "data/TheVillages.osm.pbf" -B="data/TheVillages.poly" -v --keep-tempfiles
