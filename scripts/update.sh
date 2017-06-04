3eb7da3377a9537d2fc4320cea3c8c12868a4403
Denis <carriere.denis@gmail.com>
2017-06-04 15:24:36 -0400

    Save to file

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,2 +1,2 @@
-wget http://download.geofabrik.de/north-america/us/florida-latest.osm.pbf -P data/
+wget http://download.geofabrik.de/north-america/us/florida-latest.osm.pbf -O data/florida-latest.osm.pbf
 osmupdate "data/florida-latest.osm.pbf" "data/TheVillages.osm.pbf" -B="data/TheVillages.poly" -v --keep-tempfiles
